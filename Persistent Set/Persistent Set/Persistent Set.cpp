bccbb10d4f8b3aa0b07d7a1ab97a563daf43da18
Misha Bukhalo <bukhalo1999@gmai.com>
2019-04-14 16:38:44 +0300

    Added standart reading of my class

---
+++ Persistent Set/Persistent Set/Persistent Set.cpp
@@ -0,0 +1,14 @@
+﻿
+#include "pch.h"
+#include <vector>
+#include <fstream>
+#include "MusicShop.h"
+
+int main()
+{
+	ifstream albums("Albums");
+	auto vect = readInformation(albums);
+
+	cin.get();
+	cin.get();
+}
